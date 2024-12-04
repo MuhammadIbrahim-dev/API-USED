ee4a204f5430a8602ed0015b9d88ba1ba2d318af
ibrahim-develop <muhammadibrahimmughal35@gmail.com>
2024-12-04 08:26:57 -0800

    structure

---
+++ src/Catagory.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Catagory() {
+  return (
+    <>
+    <div className='ml-[20px]'>
+      <p className='p-[10px] text-[40px] font-normal'>CATAGORY</p>
+      <ul>
+        
+      <li className='bg-[gray] text-[white] p-[10px] mb-[10px] hover:bg-[black] transition-all 0.2s ease-in'>catagory 1</li>
+      <li className='bg-[gray] text-[white] p-[10px] mb-[10px] hover:bg-[black] transition-all 0.2s ease-in'>catagory 1</li>
+      <li className='bg-[gray] text-[white] p-[10px] mb-[10px] hover:bg-[black] transition-all 0.2s ease-in'>catagory 1</li>
+      <li className='bg-[gray] text-[white] p-[10px] mb-[10px] hover:bg-[black] transition-all 0.2s ease-in'>catagory 1</li>
+      <li className='bg-[gray] text-[white] p-[10px] mb-[10px] hover:bg-[black] transition-all 0.2s ease-in'>catagory 1</li>
+      <li className='bg-[gray] text-[white] p-[10px] mb-[10px] hover:bg-[black] transition-all 0.2s ease-in'>catagory 1</li>
+      </ul>
+    </div>
+    </>
+  )
+}
